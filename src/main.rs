0d4a2c8ea1d5e7f2b241043a2f3df125eef02100
lin.guo <lin.guo@chaitin.com>
2023-05-07 15:02:57 +0800

    println! params

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,20 @@ use ferris_says::say;
 use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    let x = 6;
+    println!("{x}");
+    let x = 5;
+    println!("{}", x)
 }
+
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello fellow Rustaceans!");
+//     let width = message.chars().count();
+
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(message.as_bytes(), width, &mut writer).unwrap();
+// }
 
 // // use ferris_says::say; // from the previous step
 // // use std::io::{stdout, BufWriter};
