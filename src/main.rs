0d64ebc7e9601677df1063a61a83375d0bd7a30b
lin.guo <lin.guo@chaitin.com>
2023-05-12 10:41:46 +0800

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-struct User {
-    name: String,
-    age: i32,
-    is_student: bool,
-}
-
 fn main() {
-    let user1 = User {
-        name: String::from("rust"),
-        age: 5,
-        is_student: false,
-    };
-    println!("main: {}", user1.name);
+    println!("hello rust");
 }
