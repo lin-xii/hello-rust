ca1ec64e3da2a18929918ca98ef292d84230026a
lin.guo <lin.guo@chaitin.com>
2023-05-12 14:31:03 +0800

    mut scalar: 修改的是同地址的数据. addr不发生变化

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,25 @@
 use std::println;
 
 fn main() {
-    // scalar
-    let type_bool = true; // bool 这个没什么疑问
-    println!("type_bool: {}", type_bool);
+    /* scalar: bool char */
+    // let type_bool = true; // bool 这个没什么疑问
+    // println!("type_bool: {}", type_bool);
+    // let type_char: char = 'c'; // 字符, 仅一个. 4Byte == 32bit
+    //                            // Byte is B. bit is b.
+    // println!("type_char: {}", type_char);
 
-    let type_char: char = 'c'; // 字符, 仅一个. 4Byte == 32bit
-                               // Byte is B. bit is b.
-    println!("type_char: {}", type_char);
+    /* conclusion: mut scalar, 修改的是内存地址内的数据. 值变, 但内存地址相同 */
+    // let mut mut_num = 3;
+    // println!("mut i32's addr: {:p}", &mut_num); // mut scalar's addr: 0x16f35ea24
+    // mut_num = 5;
+    // println!("i32 changed. Had addr changed?: {:p}", &mut_num); // i32 changed. Had addr changed?: 0x16f35ea24
 
-    // unsure: char &str String 区别?
+    /* unsure: char &str String difference? */
     let type_str = "slice"; // 切片类型 slice. 数据也是在stack的
-    let hello = "hello rust!"; // 也就是说, slice相当于不可变字符串
+    let mut hello = "hello rust!"; // 也就是说, slice相当于不可变字符串
+    println!("addr: {:p}", hello);
+    hello = "hello rust cc";
+    println!("changed addr: {:p}", hello);
     console_debug(type_str);
 
     let mut type_string = String::from("hello rust");
