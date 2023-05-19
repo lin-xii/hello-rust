e1c6b9b937bf334fec55a677d970005e5a92a47a
lin.guo <lin.guo@chaitin.com>
2023-05-19 17:03:32 +0800

    array

--- src/main.rs
+++ src/main.rs
@@ -40,4 +40,10 @@ fn main() {
     integer 根据长度变化
      */
     let pointer_size = true;
+
+    let arr = [10, 20, 30, 40, 50];
+
+    for element in arr {
+        println!("the value is: {element}");
+    }
 }
