0d1259197ec6772ef176788304b3f24c720a1bad
LinXII <lin.guo@chaitin.com>
2023-05-19 18:34:58 +0800

    摄氏度 华氏度互相转换

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 use std::println;
 
-fn main() {}
+fn main() {
+    let result = celsius_to_fahrenheit(&28);
+    println!("result: {}", result)
+}
+
+fn celsius_to_fahrenheit(celsius: &i32) -> i32 {
+    (celsius + 40) * 9 / 5
+}
+
+fn fahrenheit_to_celsius(fahrenheit: &i32) -> i32 {
+    fahrenheit * 5 / 9
+}
