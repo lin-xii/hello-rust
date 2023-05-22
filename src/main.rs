16c0fceefd0fd1dc07d4d89ae0a41080076a1323
LinXII <lin.guo@chaitin.com>
2023-05-22 19:01:49 +0800

    init Option

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,1 @@
-#[derive(Debug)]
-enum IpAddr {
-    V4(String),
-    V6(String),
-}
-fn main() {
-    let home = IpAddr::V4(String::from("127.0.0.1"));
-    let loopback = IpAddr::V6(String::from("::1"));
-
-    dbg!(home);
-    dbg!(loopback);
-}
+fn main() {}
