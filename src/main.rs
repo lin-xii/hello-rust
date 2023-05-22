1faf930e42e4349b3fdebe23db812e9cf0d0845f
LinXII <lin.guo@chaitin.com>
2023-05-22 16:00:37 +0800

    计算面积v0.0.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-use std::println;
+fn main() {
+    let width = 30;
+    let height = 50;
 
-fn main() {}
+    let area_rectangle = area(width, height);
+    dbg!(area_rectangle);
+}
+
+fn area(witdh: u32, height: u32) -> u32 {
+    witdh * height
+}
