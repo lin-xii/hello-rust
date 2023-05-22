aaea0b8e020a3ce3d594fec5c051eda425a59ba0
LinXII <lin.guo@chaitin.com>
2023-05-22 16:21:53 +0800

    dbg! 会move ownership

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 /// 长方形
+#[derive(Debug)]
 struct Rectangle {
     /// 宽度
     width: u32,
@@ -14,7 +15,11 @@ fn main() {
     let area_rectangle = area(&rect1);
 
     // Output
+    println!("rectangle is:{:#?}", rect1);
     dbg!(area_rectangle);
+    // dbg! 会拿走ownership, 打印的时候, 需要考虑这个问题
+    dbg!(rect1);
+    println!("{:#?}", rect1);
 }
 
 fn area(rectangle: &Rectangle) -> u32 {
