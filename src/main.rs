aa472393494cb1807a8011a4732892724d49a18c
LinXII <lin.guo@chaitin.com>
2023-05-22 14:24:42 +0800

    体验GitHub copilot chat

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::println;
 
 fn main() {
     let mut a = 1;
-    println!("before: {a}");
+    println!("before: {:p}", &a);
     plus_one(&mut a);
     println!("after: {}", a)
 }
@@ -13,4 +13,5 @@ fn main() {
 // scalar需要配合deref * 才能修改值
 fn plus_one(num: &mut i32) {
     *num = *num + 1;
+    println!("plus_one: {:p}", num);
 }
