af74f43ec140b791d6fd569284411226093bee68
LinXII <lin.guo@chaitin.com>
2023-05-30 14:10:01 +0800

    结构体的lifetime

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-fn main() {
-    let string1 = String::from("abcd");
-    let string2 = "xyz";
-
-    let result = longest(string1.as_str(), string2);
-    println!("The longest string is {}", result);
+struct ImportantExcerpt<'a> {
+    part: &'a str,
 }
 
-fn longest(x: &str, y: &str) -> &'static str {
-    let result = String::from("really long string");
-    result.as_str()
+fn main() {
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportantExcerpt {
+        part: first_sentence,
+    };
 }
