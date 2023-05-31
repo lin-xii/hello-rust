339f49f1f0dd25c03bd7d31602e81a0615481e92
LinXII <lin.guo@chaitin.com>
2023-05-31 17:11:28 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -46,8 +46,19 @@ fn main() {
     let list = vec![1, 2, 3];
     println!("Before defining closure: {:?}", list);
 
-    let s = example_closure(String::from("hello"));
-    let n = example_closure(5);
+    let user_pref1 = Some(ShirtColor::Red);
+    let giveaway1 = store.giveaway(user_pref1);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref1, giveaway1
+    );
+
+    let user_pref2 = None;
+    let giveaway2 = store.giveaway(user_pref2);
+    println!(
+        "The user with preference {:?} gets {:?}",
+        user_pref2, giveaway2
+    );
 }
 
 // 改点代码, 试试rebase
