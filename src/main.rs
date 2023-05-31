b2f1b2d2bb9bb48f6a78563a5843ace9cf1ce083
LinXII <lin.guo@chaitin.com>
2023-05-31 17:11:28 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,38 @@ struct User {
     // 只能说rustc, 是真的强
     age: i32,
 }
+#[derive(Debug, PartialEq, Copy, Clone)]
+enum ShirtColor {
+    Red,
+    Blue,
+}
+
+struct Inventory {
+    shirts: Vec<ShirtColor>,
+}
+
+impl Inventory {
+    fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
+        user_preference.unwrap_or_else(|| self.most_stocked())
+    }
+
+    fn most_stocked(&self) -> ShirtColor {
+        let mut num_red = 0;
+        let mut num_blue = 0;
+
+        for color in &self.shirts {
+            match color {
+                ShirtColor::Red => num_red += 1,
+                ShirtColor::Blue => num_blue += 1,
+            }
+        }
+        if num_red > num_blue {
+            ShirtColor::Red
+        } else {
+            ShirtColor::Blue
+        }
+    }
+}
 
 fn main() {
     // doc: 验证patch
